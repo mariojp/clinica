cf5e9bb1f433860ea38e59e99719bb05821cc2e3
Mario Jorge Pereira <mariojp@gmail.com>
2021-05-12 21:21:58 -0300

    fix: port 80

--- src/main/java/br/com/med/clinica/ClinicaApplication.java
+++ src/main/java/br/com/med/clinica/ClinicaApplication.java
@@ -3,6 +3,7 @@ package br.com.med.clinica;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -15,7 +16,7 @@ public class ClinicaApplication {
 	}
 
 	
-	@RequestMapping("/")
+	@GetMapping("/")
     @ResponseBody
     public String home() {
       return "Hello World!";
