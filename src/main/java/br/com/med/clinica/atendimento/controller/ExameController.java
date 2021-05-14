7b0d6aefd16b7da84b05b1438edb46760845df2f
TarcisioOliveira2021 <79255361+TarcisioOliveira2021@users.noreply.github.com>
2021-05-14 14:54:58 -0300

    Uptade ExameController e ExameRepository

--- src/main/java/br/com/med/clinica/atendimento/controller/ExameController.java
+++ src/main/java/br/com/med/clinica/atendimento/controller/ExameController.java
@@ -1,5 +1,50 @@
 package br.com.med.clinica.atendimento.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.repository.query.Param;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import br.com.med.clinica.atendimento.model.Exame;
+import br.com.med.clinica.atendimento.repository.ExameRepository;
+
 public class ExameController {
 
+	private ExameRepository exameRepository;
+	
+	@GetMapping("/exame")
+	public String listExame(Model model) {
+		List<Exame> exames = exameRepository.findAll();
+		model.addAttribute("exame", exames);
+		return "/atendimento/exame";
+	}
+
+	@GetMapping("/exame/form")
+	public String form(Model model, @Param(value = "id") Long id) {
+		Exame exame = new Exame();
+		if (id != null) {
+			Optional<Exame> op = exameRepository.findById(id);
+			if (op.isPresent()) {
+				exame = op.get();
+			}
+		}
+		model.addAttribute("Exame", exame);
+		
+		return "/atendimento/exameform";
+	}
+
+	@PostMapping("/exame/salvar")
+	public String salvar(Exame exame) {
+		exameRepository.save(exame);
+		return "redirect:/exame";
+	}
+
+	@GetMapping("/exame/delete")
+	public String delete(Long id) {
+		exameRepository.deleteById(id);
+		return "redirect:/exame";
+	}
 }
--- src/main/java/br/com/med/clinica/atendimento/repository/ExameRepository.java
+++ src/main/java/br/com/med/clinica/atendimento/repository/ExameRepository.java
@@ -1,5 +1,17 @@
 package br.com.med.clinica.atendimento.repository;
 
-public class ExameRepository {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import br.com.med.clinica.atendimento.model.Exame;
+
+@Repository
+public interface ExameRepository extends CrudRepository<Exame,Long>{
+
+	List<Exame>findAll();
+	
+	Optional<Exame> findById(Long id);
+	
 }
