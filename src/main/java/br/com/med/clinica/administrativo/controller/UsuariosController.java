aa356832cb0b5acaed1e85075af9f261dc0c8ca5
ccaiomacedo <caio.macedo@ucsal.edu.br>
2021-05-19 16:44:16 -0300

    Fixing error in the Usuarioscontroller class

--- src/main/java/br/com/med/clinica/administrativo/controller/UsuariosController.java
+++ src/main/java/br/com/med/clinica/administrativo/controller/UsuariosController.java
@@ -3,10 +3,11 @@ package br.com.med.clinica.administrativo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
-import br.com.med.clinica.administrativo.model.Usuarios;
+import br.com.med.clinica.administrativo.repository.UsuarioRepository;
+
 
 @Controller
 public class UsuariosController {
 	@Autowired
-	private Usuarios usuarios;
+	private UsuarioRepository usuarioRepository;
 }
