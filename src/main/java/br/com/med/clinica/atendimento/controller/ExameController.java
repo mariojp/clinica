ef8a015476b61c468ddf245c704ef4586b6ee081
lucasagw <lucasassuncao2014@gmail.com>
2021-05-19 10:58:03 -0300

    ajustes...

--- src/main/java/br/com/med/clinica/atendimento/controller/ExameController.java
+++ src/main/java/br/com/med/clinica/atendimento/controller/ExameController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +17,7 @@ import br.com.med.clinica.atendimento.model.Exame;
 import br.com.med.clinica.atendimento.repository.AtendimentoRepository;
 import br.com.med.clinica.atendimento.repository.ExameRepository;
 
+@Controller
 public class ExameController {
 
 	@Autowired
