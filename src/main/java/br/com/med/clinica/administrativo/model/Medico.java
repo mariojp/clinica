5bd178b1cc9e664d0a4ce140d9f2c4b9bf1f743f
bruno pinho <iaishowcs@gmail.com>
2021-06-08 18:00:15 -0300

    Medico model atualizado

--- src/main/java/br/com/med/clinica/administrativo/model/Medico.java
+++ src/main/java/br/com/med/clinica/administrativo/model/Medico.java
@@ -1,74 +1,171 @@
 package br.com.med.clinica.administrativo.model;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotEmpty;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 
-
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Medico.
+ */
 @Entity
 @Table(name = "tb_medico")
 public class Medico {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long oid;
+	/** The oid. */
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long oid;
 
-    @NotEmpty(message = "Não pode ser vazio")
-    private String concelho;
+	/** The concelho. */
+	@NotBlank(message = "NAO pode ser vazio")
+	private String concelho;
 
-    @OneToOne
-    @MapsId
-    private Funcionario funcionario;
+	/** The funcionario. */
+	@OneToOne
+	@MapsId
+	private Funcionario funcionario;
 
-    @ManyToOne
-    @JoinColumn(name = "especialidade_oid")
-    private Especialidade especialidade;
+	/** The especialidade. */
+	@ManyToOne
+	@JoinColumn(name = "especialidade_oid")
+	private Especialidade especialidade;
 
+	/**
+	 * Instantiates a new medico.
+	 */
+	public Medico() {
+		super();
+	}
 
+	/**
+	 * Instantiates a new medico.
+	 *
+	 * @param funcionario the funcionario
+	 */
+	public Medico(Funcionario funcionario) {
+		this.funcionario = funcionario;
+	}
 
-    public Long getOid() {
-        return oid;
-    }
+	/**
+	 * Instantiates a new medico.
+	 *
+	 * @param concelho the concelho
+	 * @param funcionario the funcionario
+	 * @param especialidade the especialidade
+	 */
+	public Medico(String concelho, Funcionario funcionario, Especialidade especialidade) {
+		this.concelho = concelho;
+		this.funcionario = funcionario;
+		this.especialidade = especialidade;
+	}
 
-    public void setOid(Long oid) {
-        this.oid = oid;
-    }
+	/**
+	 * Gets the oid.
+	 *
+	 * @return the oid
+	 */
+	public Long getOid() {
+		return oid;
+	}
 
-    public String getConcelho() {
-        return concelho;
-    }
+	/**
+	 * Sets the oid.
+	 *
+	 * @param oid the new oid
+	 */
+	public void setOid(Long oid) {
+		this.oid = oid;
+	}
 
-    public void setConcelho(String concelho) {
-        this.concelho = concelho;
-    }
+	/**
+	 * Gets the concelho.
+	 *
+	 * @return the concelho
+	 */
+	public String getConcelho() {
+		return concelho;
+	}
 
-    public Funcionario getFuncionario() {
-        return funcionario;
-    }
+	/**
+	 * Sets the concelho.
+	 *
+	 * @param concelho the new concelho
+	 */
+	public void setConcelho(String concelho) {
+		this.concelho = concelho;
+	}
 
-    public void setFuncionario(Funcionario funcionario) {
-        this.funcionario = funcionario;
-    }
+	/**
+	 * Gets the funcionario.
+	 *
+	 * @return the funcionario
+	 */
+	public Funcionario getFuncionario() {
+		return funcionario;
+	}
 
-    public Especialidade getEspecialidade() {
-        return especialidade;
-    }
+	/**
+	 * Sets the funcionario.
+	 *
+	 * @param funcionario the new funcionario
+	 */
+	public void setFuncionario(Funcionario funcionario) {
+		this.funcionario = funcionario;
+	}
 
-    public void setEspecialidade(Especialidade especialidade) {
-        this.especialidade = especialidade;
-    }
+	/**
+	 * Gets the especialidade.
+	 *
+	 * @return the especialidade
+	 */
+	public Especialidade getEspecialidade() {
+		return especialidade;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Medico)) return false;
+	/**
+	 * Sets the especialidade.
+	 *
+	 * @param especialidade the new especialidade
+	 */
+	public void setEspecialidade(Especialidade especialidade) {
+		this.especialidade = especialidade;
+	}
 
-        Medico medico = (Medico) o;
+	/**
+	 * Equals.
+	 *
+	 * @param o the o
+	 * @return true, if successful
+	 */
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (!(o instanceof Medico))
+			return false;
 
-        return getOid().equals(medico.getOid());
-    }
+		Medico medico = (Medico) o;
 
-    @Override
-    public int hashCode() {
-        return getOid().hashCode();
-    }
+		return getOid().equals(medico.getOid());
+	}
+
+	/**
+	 * Hash code.
+	 *
+	 * @return the int
+	 */
+	@Override
+	public int hashCode() {
+		return getOid().hashCode();
+	}
+
 }
+
