36060954e30d377a9181eacb62be0aaecedb4f7a
ricardoliveiraof2m <ricardoliveira0703@gmail.com>
2021-06-08 18:12:15 -0300

    UsuarioRepository Update

--- src/main/java/br/com/med/clinica/administrativo/repository/UsuarioRepository.java
+++ src/main/java/br/com/med/clinica/administrativo/repository/UsuarioRepository.java
@@ -1,14 +1,14 @@
 package br.com.med.clinica.administrativo.repository;
 
-import br.com.med.clinica.administrativo.model.Usuario;
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import br.com.med.clinica.administrativo.model.Usuario;
 
 @Repository
 public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
 
-
-    List<Usuario> findAll();
+	List<Usuario> findAll();
 }
