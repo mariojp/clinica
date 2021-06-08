c7625c5f2c1d428f14989419af8f1304af2b57a6
bruno pinho <iaishowcs@gmail.com>
2021-06-08 17:59:38 -0300

    Model Usuario atualizado

--- src/main/java/br/com/med/clinica/administrativo/model/Usuario.java
+++ src/main/java/br/com/med/clinica/administrativo/model/Usuario.java
@@ -1,74 +1,164 @@
 package br.com.med.clinica.administrativo.model;
 
-
-import javax.persistence.*;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Size;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Usuario.
+ */
 @Entity
 @Table(name = "tb_usuario")
 public class Usuario {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long oid;
+	/** The oid. */
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long oid;
 
-    @Size(max = 15, min = 2, message = "O usuário deve ter de 2 a 15 Caracteres")
-    private String Username;
+	/** The username. */
+	@NotBlank(message = "CAMPO OBRIGATÓRIO - NAO PODE ESTAR VAZIO")
+	@Size(max = 15, min = 2, message = "O usuário deve ter de 2 a 15 Caracteres")
+	private String username;
 
-    @javax.validation.constraints.Email
-    private String Email;
+	/** The email. */
+	@NotBlank(message = "CAMPO OBRIGATÓRIO - NAO PODE ESTAR VAZIO")
+	@Email
+	private String email;
 
-    @Size(max = 8, min = 4, message = "A senha deve ser de 4 a 8 digitos")
-    private String senha;
+	/** The password. */
+	@NotBlank(message = "CAMPO OBRIGATÓRIO - NAO PODE ESTAR VAZIO")
+	@Size(max = 8, min = 4, message = "A senha deve ser de 4 a 8 digitos")
+	private String password;
 
-    @OneToOne
-    private Funcionario funcionario;
+	/** The funcionario. */
+	@OneToOne
+	@MapsId
+	private Funcionario funcionario;
 
-    public Long getOid() {
-        return oid;
-    }
+	/**
+	 * Gets the oid.
+	 *
+	 * @return the oid
+	 */
+	public Long getOid() {
+		return oid;
+	}
 
-    public void setOid(Long oid) {
-        this.oid = oid;
-    }
+	/**
+	 * Sets the oid.
+	 *
+	 * @param oid the new oid
+	 */
+	public void setOid(Long oid) {
+		this.oid = oid;
+	}
 
-    public String getUsername() {
-        return Username;
-    }
+	/**
+	 * Gets the username.
+	 *
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
 
-    public void setUsername(String username) {
-        Username = username;
-    }
+	/**
+	 * Gets the funcionario.
+	 *
+	 * @return the funcionario
+	 */
+	public Funcionario getFuncionario() {
+		return funcionario;
+	}
 
-    public String getEmail() {
-        return Email;
-    }
+	/**
+	 * Sets the funcionario.
+	 *
+	 * @param funcionario the new funcionario
+	 */
+	public void setFuncionario(Funcionario funcionario) {
+		this.funcionario = funcionario;
+	}
 
-    public void setEmail(String email) {
-        Email = email;
-    }
+	/**
+	 * Sets the username.
+	 *
+	 * @param username the new username
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
-    public String getSenha() {
-        return senha;
-    }
+	/**
+	 * Gets the email.
+	 *
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
 
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
+	/**
+	 * Sets the email.
+	 *
+	 * @param email the new email
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Usuario usuario = (Usuario) o;
-        return oid.equals(usuario.oid);
-    }
+	/**
+	 * Gets the password.
+	 *
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(oid);
-    }
+	/**
+	 * Sets the password.
+	 *
+	 * @param senha the new password
+	 */
+	public void setPassword(String senha) {
+		this.password = senha;
+	}
+
+	/**
+	 * Equals.
+	 *
+	 * @param o the o
+	 * @return true, if successful
+	 */
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Usuario usuario = (Usuario) o;
+		return oid.equals(usuario.oid);
+	}
+
+	/**
+	 * Hash code.
+	 *
+	 * @return the int
+	 */
+	@Override
+	public int hashCode() {
+		return Objects.hash(oid);
+	}
 }
