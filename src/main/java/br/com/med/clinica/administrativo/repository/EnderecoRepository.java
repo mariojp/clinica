fa7019bf4d57bc97da8f434a902b54e529d29da4
ricardoliveiraof2m <ricardoliveira0703@gmail.com>
2021-06-08 18:07:58 -0300

    EnderecoRepository Update

--- src/main/java/br/com/med/clinica/administrativo/repository/EnderecoRepository.java
+++ src/main/java/br/com/med/clinica/administrativo/repository/EnderecoRepository.java
@@ -2,11 +2,11 @@ package br.com.med.clinica.administrativo.repository;
 
 import java.util.List;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.med.clinica.administrativo.model.Endereco;
 
-public interface EnderecoRepository extends CrudRepository<Endereco, Long>{
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
 
 	List<Endereco> findAll();
 }
