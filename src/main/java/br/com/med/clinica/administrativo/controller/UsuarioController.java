1c24ddf81940c83fc7e765fcbd188e83aab694fb
Carlos Laurine <carlos.pinho@ucsal.edu.br>
2021-06-08 17:54:01 -0300

    Usuario Controller Update

--- src/main/java/br/com/med/clinica/administrativo/controller/UsuarioController.java
+++ src/main/java/br/com/med/clinica/administrativo/controller/UsuarioController.java
@@ -1,6 +1,10 @@
 package br.com.med.clinica.administrativo.controller;
-import br.com.med.clinica.administrativo.model.Usuario;
-import br.com.med.clinica.administrativo.repository.UsuarioRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
@@ -8,54 +12,64 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import javax.validation.Valid;
-import java.util.List;
-import java.util.Optional;
+
+import br.com.med.clinica.administrativo.model.Funcionario;
+import br.com.med.clinica.administrativo.model.Usuario;
+import br.com.med.clinica.administrativo.repository.FuncionarioRepository;
+import br.com.med.clinica.administrativo.repository.UsuarioRepository;
 
 @Controller
 public class UsuarioController {
 
-    @Autowired
-    private UsuarioRepository usuarioRepository;
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	@Autowired
+	private FuncionarioRepository funcionarioRepository;
 
-    @GetMapping("/usuario")
-    public String listUsuario(Model model) {
-        List<Usuario> usuarios =  usuarioRepository.findAll();
-        model.addAttribute("usuarios",usuarios);
-        return "/administrativo/usuariolist";
+	@GetMapping("/usuario")
+	public String listUsuario(Model model) {
+		List<Usuario> usuarios = usuarioRepository.findAll();
+		model.addAttribute("usuarios", usuarios);
+		return "/administrativo/usuario";
 
-    }
+	}
 
-    @GetMapping("/usuario/form")
-    public String form(Model model,@Param(value = "id") Long id) {
-        Usuario usuario = new Usuario();
-        if(id != null) {
-            Optional<Usuario> op = usuarioRepository.findById(id);
-            if(op.isPresent()) {
-                usuario = op.get();
-            }
-        }
-        model.addAttribute("Usuario",usuarioRepository);
+	@GetMapping("/usuario/form")
+	public String form(Model model, @Param(value = "id") Long id) {
+		Usuario usuario = new Usuario();
+		if (id != null) {
+			Optional<Usuario> op = usuarioRepository.findById(id);
+			if (op.isPresent()) {
+				usuario = op.get();
+			}
+		}
+		List<Funcionario> funcionarios = funcionarioRepository.findAll();
+		model.addAttribute("funcionarios", funcionarios);
+		model.addAttribute("usuario", usuario);
 
-        return "/administrativo/usuarioform";
+		return "/administrativo/usuarioform";
+	}
 
-    }
+	@PostMapping("/usuario/salvar")
+	public String salvar(@Valid Usuario entity, BindingResult bindingResult) {
 
-    @PostMapping("/usuario/salvar")
-    public String salvar(@Valid Usuario usuario, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            return "/administrativo/usuarioform";
-        }
-        usuarioRepository.save(usuario);
-        return "redirect:/usuario";
-    }
+		if (bindingResult.hasErrors()) {
+			return "/administrativo/usuarioform";
+		}
+		Usuario usuario = new Usuario();
+		usuario.setUsername(entity.getUsername());
+		usuario.setPassword(entity.getPassword());
+		usuario.setEmail(entity.getEmail());
+		usuario.setOid(entity.getOid());
+		usuario.setFuncionario(funcionarioRepository.findById(entity.getFuncionario().getOid()).get());
+		usuarioRepository.save(usuario);
+		return "redirect:/usuario";
+	}
 
-
-    @GetMapping("/usuario/delete")
-    public String delete(Long id) {
-        usuarioRepository.deleteById(id);
-        return "redirect:/usuario";
-    }
-
+	@GetMapping("/usuario/delete")
+	public String delete(Long id) {
+		usuarioRepository.deleteById(id);
+		return "redirect:/usuario";
+	}
 
 }
