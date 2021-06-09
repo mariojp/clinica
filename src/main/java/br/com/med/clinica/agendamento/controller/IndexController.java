24f96c316f2e6369e40cd642860e64efe0bc5b04
LaraFer10 <lara.ferrer001@gmail.com>
2021-06-08 23:34:12 -0300

    add home link

--- src/main/java/br/com/med/clinica/agendamento/controller/IndexController.java
+++ src/main/java/br/com/med/clinica/agendamento/controller/IndexController.java
@@ -1,13 +1,24 @@
 package br.com.med.clinica.agendamento.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import br.com.med.clinica.agendamento.model.Usuario;
 
 @Controller
 public class IndexController {
 
 	@GetMapping("")
-	public String Get() {
+	public String Get(Model model) {
+		Usuario usuario = new Usuario();
+		model.addAttribute("usuario", usuario);
 		return "/agendamento/index";
+	}
+	
+	@GetMapping("/home")
+	public String getHome() {
+		
+		return "/agendamento/home";
 	}
 }
